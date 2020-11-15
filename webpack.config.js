13cce6874dfc81ec2a8d395a8c63986d9f31d2d7
Dejan Antic <dex.antic@gmail.com>
2020-11-15 19:53:17 +0100

    Change name from dist to build

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './app/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'index_bundle.js',
     publicPath: '/'
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, 'build'),
     compress: true,
     port: 9000,
     historyApiFallback: true
@@ -24,6 +25,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './app/index.html'
-    })
+    }),
+    new CopyPlugin({ patterns: [{ from: '_redirects'}]})
   ]
 }
